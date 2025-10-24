46b2acc8e842eebe6b573acc9e9b73524dc52086
kelvin <kelvin@adaremit.co.id>
2025-10-24 14:39:43 +0700

    feat(tenant): add get detail tenant use case and controller to tenant module

--- src/di/module/tenant.module.ts
+++ src/di/module/tenant.module.ts
@@ -1,6 +1,7 @@
 import { createModule } from '@evyweb/ioctopus'
 import { DI_SYMBOLS } from '@/di/types'
 import { createTenantController } from '@/modules/tenant/controller/create-tenant.controller'
+import { GetDetailTenantController } from '@/modules/tenant/controller/get-detail-tenant.controller'
 import { getInfiniteTenantListController } from '@/modules/tenant/controller/get-infinite-tenant-list.controller'
 import { getPaginatedTenantListController } from '@/modules/tenant/controller/get-paginated-tenant-list.controller'
 import { updateTenantController } from '@/modules/tenant/controller/update-tenant.controller'
@@ -8,6 +9,7 @@ import { createTenantOrchestrator } from '@/modules/tenant/orchestrator/create-t
 import { updateTenantOrchestrator } from '@/modules/tenant/orchestrator/update-tenant.orchestrator'
 import { TenantRepository } from '@/modules/tenant/tenant.repository'
 import { createTenantUseCase } from '@/modules/tenant/use-cases/create-tenant.use-case'
+import { getDetailTenantUseCase } from '@/modules/tenant/use-cases/get-detail-tenant.use-case'
 import { getInfiniteTenantListUseCase } from '@/modules/tenant/use-cases/get-infinite-tenant-list.use-case'
 import { getPaginatedTenantListUseCase } from '@/modules/tenant/use-cases/get-paginated-tenant-list.use-case'
 import { updateTenantUseCase } from '@/modules/tenant/use-cases/update-tenant.use-case'
@@ -36,6 +38,10 @@ export function createTenantModule() {
   tenantModule
     .bind(DI_SYMBOLS.IGetInfiniteTenantListUseCase)
     .toHigherOrderFunction(getInfiniteTenantListUseCase, [DI_SYMBOLS.ITenantRepository])
+
+  tenantModule
+    .bind(DI_SYMBOLS.IGetDetailTenantUseCase)
+    .toHigherOrderFunction(getDetailTenantUseCase, [DI_SYMBOLS.ITenantRepository])
 
   // orchestrators
   tenantModule
@@ -68,6 +74,10 @@ export function createTenantModule() {
 
   tenantModule.bind(DI_SYMBOLS.IGetInfiniteTenantListController).toHigherOrderFunction(getInfiniteTenantListController, {
     getInfiniteTenantListUseCase: DI_SYMBOLS.IGetInfiniteTenantListUseCase,
+  })
+
+  tenantModule.bind(DI_SYMBOLS.IGetDetailTenantController).toHigherOrderFunction(GetDetailTenantController, {
+    getDetailTenantUseCase: DI_SYMBOLS.IGetDetailTenantUseCase,
   })
 
   return tenantModule
